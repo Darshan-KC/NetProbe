b9171b01b64950c46feb735ed7d50bdd1e70b8e1
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-05-19 21:52:17 +0545

    Feat: Sweeper output file added

---
+++ sweeper/output.py
@@ -0,0 +1,18 @@
+try:
+    from colorama import Fore, Style, init
+    init(autoreset=True)
+    COLOR_ENABLED = True
+except ImportError:
+    COLOR_ENABLED = False
+
+
+def print_results(results, show_all=False):
+    for ip, status in results.items():
+        if status or show_all:
+            status_text = "Alive" if status else "Dead"
+            if COLOR_ENABLED:
+                color = Fore.GREEN if status else Fore.RED
+                print(f"{color}{ip} - {status_text}")
+            else:
+                print(f"{ip} - {status_text}")
+    # print_summary(results)
