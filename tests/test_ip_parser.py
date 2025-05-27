ec86edd713e896c0122dea9edb543d35b73f133c
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-05-27 20:42:51 +0545

    Feat: Function to test parse_cidr_valid

--- tests/test_ip_parser.py
+++ tests/test_ip_parser.py
@@ -1,2 +1,7 @@
 import pytest
 from sweeper.ip_parser import parse_cidr, parse_range
+
+def test_parse_cidr_valid():
+    result = parse_cidr("192.168.1.0/30")
+    expected = ["192.168.1.1", "192.168.1.2"]
+    assert result == expected
