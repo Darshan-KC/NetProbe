c4a67df3b4366b13555198fbfaaceb76739365bb
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-06-09 20:17:01 +0545

    Feat: run file updated

--- run.py
+++ run.py
@@ -1,20 +1,26 @@
-from sweeper import parse_cidr, parse_range, scan_ips, print_results
+# from sweeper import parse_cidr, parse_range, scan_ips, print_results
 
-def demo():
-    # Example input (you can change this to any CIDR or IP range)
-    cidr_input = "192.168.1.0/24"  # Gives 192.168.1.1 and 192.168.1.2
+# def demo():
+#     # Example input (you can change this to any CIDR or IP range)
+#     cidr_input = "192.168.1.0/24"  # Gives 192.168.1.1 and 192.168.1.2
 
-    print("[+] Parsing CIDR range...")
-    ip_list = parse_cidr(cidr_input)
+#     print("[+] Parsing CIDR range...")
+#     ip_list = parse_cidr(cidr_input)
 
-    print(f"[+] Scanning {len(ip_list)} hosts...")
-    results = scan_ips(ip_list, threads=5)
+#     print(f"[+] Scanning {len(ip_list)} hosts...")
+#     results = scan_ips(ip_list, threads=5)
 
-    print("[+] Scan results:")
-    print_results(results)
+#     print("[+] Scan results:")
+#     print_results(results)
 
-    # print("[+] Exporting results to 'results.txt'...")
-    # export_results(results, "results.txt", filetype="txt")
+#     # print("[+] Exporting results to 'results.txt'...")
+#     # export_results(results, "results.txt", filetype="txt")
+
+# if __name__ == "__main__":
+#     demo()
+
+
+from cli.main import main
 
 if __name__ == "__main__":
-    demo()
+    main()
